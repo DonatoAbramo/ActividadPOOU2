66a9aef6a0c816cbb4eb391319e90692c413d033
DonatoAbramo <85904374+DonatoAbramo@users.noreply.github.com>
2021-07-04 12:49:03 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividad obligatoria POO Unidad 2</title>
+</head>
+
+
+
+<body>
+    
+<?php
+
+
+echo "NOMBRE: Donato";
+echo "<br>";
+echo "APELLIDO: Abramo";
+echo "<br>";
+echo "HOBBIE: Jugar al futbol";
+echo "<br>";
+echo "EDITOR DE CODIGO PREDILECTO: Visual Studio Code";
+echo "<br>";
+echo "SISTEMA OPERATIVO UTILIZADO: Windows";
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+</body>
+
+
+
+
+
+
+
+
+</html>
